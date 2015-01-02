374110b4ca6af7b7faafebfaf78b956876c54236
Matthew Love <matthewlove94@gmail.com>
2015-01-02 01:22:32 +0000

    improved string for program description

--- aes_encryption.cpp
+++ aes_encryption.cpp
@@ -83,18 +83,34 @@ void main_menu()
 			break;
 		case 4:
 			std::cout << "\n[1]: Encodes a .txt file in 128 Bit AES.\n"
-					     "[1]: User is asked to enter the name of the input and output files.\n"
-					     "[1]: The program automatically adds the .txt extension.\n\n"
-                         "[2]: Same as [1] but outputs the key used in a .txt file for the user in the same directory.\n\n"
-                         "[3]: Test Run: Encrypts a preset 4x4 array and outputs the results.\n\n"
-                         "State is:" 	      << "\t\tThe cipher key is:" << "\tThe results should be:\n"
-                         "[32, 88, 31, e0]" << "\t[2b, 28, ab, 09]"     << "\t[39, 02, dc, 19]\n"
-                         "[43, 5a, 31, 37]" << "\t[7e, ae, f7, cf]"     << "\t[25, dc, 11, 6a]\n"
-                         "[f6, 30, 98, 07]" << "\t[15, d2, 15, 4f]"     << "\t[84, 09, 85, 0b]\n"
-                         "[a8, 8d, a2, 34]" << "\t[16, a6, 88, 3c]"     << "\t[1d, fb, 97, 32]\n\n"
-                         "Values and expected results sourced from the following website:"
-                         "http://www.cs.bc.edu/~straubin/cs381-05/blockciphers/rijndael_ingles2004.swf\n\n"
-                         "It proves that this program works correctly.\n";
+					     "[1]: Enter the name of the input and output files.\n"
+					     "[1]: .txt extension is automatically added.\n\n"
+
+                         "[2]: Like [1] but key is also saved to a file.\n\n"
+
+                         "[3]: Test to ensure correct operation.\n\n"
+
+                         "State is:\t\tThe cipher key is:\t"
+                         "The results should be:\n"
+
+                         "[32, 88, 31, e0]\t[2b, 28, ab, 09]\t"
+                         "[39, 02, dc, 19]\n"
+
+                         "[43, 5a, 31, 37]\t[7e, ae, f7, cf]\t"
+                         "[25, dc, 11, 6a]\n"
+
+                         "[f6, 30, 98, 07]\t[15, d2, 15, 4f]\t"
+                         "[84, 09, 85, 0b]\n"
+
+                         "[a8, 8d, a2, 34]\t[16, a6, 88, 3c]\t"
+                         "[1d, fb, 97, 32]\n\n"
+
+                         "Values and expected results sourced"
+                         " from the following website:\n"
+
+                         "http://www.cs.bc.edu/~straubin/cs381-05/"
+                         "blockciphers/rijndael_ingles2004.swf\n\n"
+                         "It should prove that this program works correctly.\n";
             main_menu();
             break;
 		default:
