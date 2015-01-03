6ce6a8344d0cde3da82788c78252fd76b0ada9d3
Matthew Love <matthewlove94@gmail.com>
2015-01-03 02:44:29 +0000

    struct no longer needed

--- encrypt_struct.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef _AES_DATA_STRUCT_
-#define _AES_DATA_STRUCT_
-
-struct aes_data{
-    std::array<std::array<unsigned char, 44>, 4> round_key;
-    std::vector<std::vector<unsigned char>> state;
-};
-
-#endif // _AES_DATA_STRUCT_
